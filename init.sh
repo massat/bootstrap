3ff028558428f67df18656f3f120cc053ab09a7a
massat <masato.hirai@gmail.com>
2020-07-05 22:36:02 +0900

    fix

--- init.sh
+++ init.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 /usr/local/bin/brew install pyenv
 
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 
-pyenv install 3.7.5
-pyenv global 3.7.5
+pyenv install 3.8.3
+pyenv global 3.8.3
 pyenv rehash
 
 sudo pip3 install ansible
-sudo ansible-galaxy install zzet.rbenv
