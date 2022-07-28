a4c5380852a06b480d0b19dc0382e0ca6585e020
massat <masato.hirai@gmail.com>
2022-07-28 21:31:28 +0900

    fix

--- init.sh
+++ init.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> .zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zshrc
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 /usr/local/bin/brew install git
 
 git clone https://github.com/anyenv/anyenv ~/.anyenv
-echo 'eval "$(~/.anyenv/bin/anyenv init -)"' >> ~/.zprofile
+echo 'eval "$(~/.anyenv/bin/anyenv init -)"' >> ~/.zshrc
 eval "$(~/.anyenv/bin/anyenv init -)"
-echo 'export PATH="$HOME/.anyenv/bin:$PATH"' >> ~/.zprofile
+echo 'export PATH="$HOME/.anyenv/bin:$PATH"' >> ~/.zshrc
 
 ~/.anyenv/bin/anyenv install --init
 ~/.anyenv/bin/anyenv install nodenv
